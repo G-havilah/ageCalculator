fcc235a79ec38f9a99e5e95eb4396b01f2420c57
G-havilah <oluwadamilolaogunwale@yahoo.com>
2019-08-30 22:11:19 +0100

    ageCal.js

---
+++ ageCal.js
@@ -0,0 +1,5 @@
+var countryName = prompt("What is the name of your country")
+var pastYear = prompt("What year did you gain independence?")
+var presentYear = prompt("What year is it today?")
+var age = presentYear - pastYear;
+alert(countryName +" "+ "is" + " "+ age +"years old")
